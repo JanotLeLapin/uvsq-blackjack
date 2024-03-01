6624d218f3a8e3a082927e6161b7714f2c40c272
Joseph DALY <joseph300905@gmail.com>
2024-03-01 10:58:34 +0100

    main loop

--- main.py
+++ main.py
@@ -9,4 +9,33 @@ def pick_card():
     return STACK.pop()
 
 def deserialize_card(card):
-    return (KINDS[card // 13], card % 13)
+    return (KINDS[card // 13], min(card % 13, 10))
+
+def count_deck(deck):
+    return sum(map(lambda card: deserialize_card(card)[1] + 1, deck))
+
+def stringify_card(card):
+    (kind, value) = deserialize_card(card)
+    return f"{'As' if value == 0 else value + 1} de {kind}"
+
+def stringify_deck(deck):
+    return ", ".join(map(stringify_card, joueur)) + f" (valeur totale: {count_deck(deck)})"
+
+joueur = [pick_card() for _ in range(2)]
+croupier = [pick_card() for _ in range(2)]
+
+print(stringify_deck(joueur))
+while True:
+    option = input("> Quel est votre choix?\n").upper()
+    if option == "HIT":
+        joueur.append(pick_card())
+        print(stringify_deck(joueur))
+        if count_deck(joueur) > 21:
+            print("Degage sale clochard")
+            break
+    elif option == "STAND":
+        break
+    elif option == "SURRENDER":
+        break
+    else:
+        print("Option inconnue")
