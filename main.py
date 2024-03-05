904455bdf65686d7f9370ca8654eaa6dca6d0fc5
Joseph DALY <joseph300905@gmail.com>
2024-03-05 11:12:18 +0100

    add comments

--- main.py
+++ main.py
@@ -1,31 +1,49 @@
 import random
 
 KINDS = {0: "Carreau", 1: "Pic", 2: "Coeur", 3: "Trèfle"}
-STACK = [x for x in range(13 * 4)]
 
+# Génération de la pile
+STACK = [x for x in range(13 * 4)]
 random.shuffle(STACK)
 
 def pick_card():
+    '''
+    Retire la dernière carte de la pile et la renvoie
+    '''
     return STACK.pop()
 
 def deserialize_card(card):
+    '''
+    Renvoie un tuple contenant le type de la carte et sa valeur
+    '''
     return (KINDS[card // 13], min(card % 13, 10))
 
 def count_deck(deck):
+    '''
+    Renvoie la valeur totale du deck, ou -1 si la valeur excède 21
+    '''
     value = sum(map(lambda card: deserialize_card(card)[1] + 1, deck))
     return -1 if value > 21 else value
 
 def stringify_card(card):
+    '''
+    Renvoie une représentation lisible d'une carte
+    '''
     (kind, value) = deserialize_card(card)
     return f"{'As' if value == 0 else value + 1} de {kind}"
 
 def stringify_deck(deck):
+    '''
+    Renvoie une représentation lisible d'un deck
+    '''
     return ", ".join(map(stringify_card, joueur)) + f" (valeur totale: {count_deck(deck)})"
 
+# Génération des decks
 joueur = [pick_card() for _ in range(2)]
 croupier = [pick_card() for _ in range(2)]
-
 print(stringify_deck(joueur))
+
+# Boucle principale
 while True:
     option = input("> Quel est votre choix?\n").upper()
     if option == "HIT":
