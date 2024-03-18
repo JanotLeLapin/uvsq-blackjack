80b76bbedf97684d9f44099738d433fb7757a895
Sabri Kabani <sabri.kabani@ens.uvsq.fr>
2024-03-18 15:11:50 +0100

    finished product

--- main.py
+++ main.py
@@ -9,54 +9,51 @@ random.shuffle(STACK)
 
 
 def pick_card():
-    
-    #Retire la dernière carte de la pile et la renvoie
-    
+    # Retire la dernière carte de la pile et la renvoie
     return STACK.pop()
 
+
 def deserialize_card(card):
-    
-    #Renvoie un tuple contenant le type de la carte et sa valeur
-    
+    # Renvoie un tuple contenant le type de la carte et sa valeur
     return (KINDS[card // 13], min(card % 13, 10))
 
+
 def count_deck(deck):
-    
-    #Renvoie la valeur totale du deck, ou -1 si la valeur excède 21
-    
+    # Renvoie la valeur totale du deck, ou -1 si la valeur excède 21
     value = sum(map(lambda card: deserialize_card(card)[1] + 1, deck))
     return -1 if value > 21 else value
 
+
 def stringify_card(card):
-  
-    #Renvoie une représentation lisible d'une carte
-   
+    # Renvoie une représentation lisible d'une carte
     (kind, value) = deserialize_card(card)
     return f"{'As' if value == 0 else value + 1} de {kind}"
 
+
 def stringify_deck(deck):
-    
-    #Renvoie une représentation lisible d'un deck
-    
-    return (", ".join(map(stringify_card, joueur)) + f" (valeur totale: {count_deck(deck)})")
+    # Renvoie une représentation lisible d'un deck
+    return (", ".join(map(stringify_card, deck)) + f" (valeur totale: {count_deck(deck)})")
+
 
 # Génération des decks
 joueur = [pick_card() for _ in range(2)]
 croupier = [pick_card() for _ in range(2)]
-bot1=[pick_card() for _ in range(2)]
-bot2=[pick_card() for _ in range(2)]
-bot3=[pick_card() for _ in range(2)]
+bot1 = [pick_card() for _ in range(2)]
+bot2 = [pick_card() for _ in range(2)]
+bot3 = [pick_card() for _ in range(2)]
 
+print(bot1)
+print(bot2)
+print(bot3)
 
+# bots choisissent ce qu'ils veulent faire
 
-#bots choisissent ce qu'ils veulent faire
+botoption = ["hit","stand"]
+choixbot1 = random.randint(0,1)
+choixbot2 = random.randint(0,1)
+choixbot3 = random.randint(0,1)
 
-botoption=["hit","stand"]
-choixbot1=random.randint(0,2)
-choixbot2=random.randint(0,2)
-choixbot3=random.randint(0,2)
-
-#Choix du bot1
+# Choix du bot1
 if botoption[choixbot1]=="hit":
 
     bot1.append(pick_card())
@@ -65,10 +62,10 @@ if botoption[choixbot1]=="hit":
             print("bot1 a depassé 21: élimination du jeu")
             
 elif botoption[choixbot1]=="stand":
-    print("bot 1 ne force pas il a donc"+ stringify_card(bot1))
+    print("bot 1 ne force pas il a donc "+ stringify_deck(bot1))
 
 
-#Choix du bot2
+# Choix du bot2
 if botoption[choixbot2]=="hit":
     bot2.append(pick_card())
     print(stringify_deck(bot2))
@@ -76,10 +73,10 @@ if botoption[choixbot2]=="hit":
             print("bot2 a depassé 21: élimination du jeu")
             
 elif botoption[choixbot2]=="stand":
-    print("bot 2 ne force pas il a donc"+ stringify_card(bot2))
+    print("bot 2 ne force pas il a donc "+ stringify_deck(bot2))
     
 
-#Choix du bot3
+# Choix du bot3
 if botoption[choixbot3]=="hit":
     bot3.append(pick_card())
     print(stringify_deck(bot3))
@@ -87,7 +84,7 @@ if botoption[choixbot3]=="hit":
             print("bot3 a depassé 21: élimination du jeu")
             
 elif botoption[choixbot3]=="stand":
-    print("bot 3 ne force pas il a donc"+ stringify_card(bot3))
+    print("bot 3 ne force pas il a donc "+ stringify_deck(bot3))
     
 # Boucle principale
  
@@ -126,36 +123,4 @@ bot2res=count_deck(bot2) - count_croupier
 bot3res=count_deck(bot3) - count_croupier
 
 print(f"Deck croupier: {count_croupier}")
-
-def resultat():
-    if res > 0:
-        print("Joueur a gagné")
-    elif res == 0:
-        print("Égalité")
-    else:
-        print("Joueur a perdu")
-
-
-if bot1res>0:
-    print("Bot1 a gagné avec une valeur de " + count_deck(bot1))
-elif bot1res == 0:
-    print("Égalité avec une valeur de " + count_deck(bot1))
-else:
-    print("Bot1 a perdu avec une valeur de " + count_deck(bot1))
-
-
-if bot2res>0:
-    print("Bot1 a gagné avec une valeur de " + count_deck(bot2))
-elif bot2res == 0:
-    print("Égalité avec une valeur de " + count_deck(bot2))
-else:
-    print("Bot1 a perdu avec une valeur de " + count_deck(bot2))
-
-
-if bot3res>0:
-    print("Bot1 a gagné avec une valeur de " + count_deck(bot3))
-elif bot3res == 0:
-    print("Égalité avec une valeur de " + count_deck(bot3))
-else:
-    print("Bot1 a perdu avec une valeur de " + count_deck(bot3))
 
